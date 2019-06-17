273381c259fc3daf6cf3167b7411a7708ad46d8b
Diana Calpa <dp.calpa251@gmail.com>
2019-06-17 15:28:29 -0500

    Fixes3

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -31,13 +31,13 @@ export class SidebarComponent implements OnInit {
   // Estilos
   stylesHidro = {
     hidroEmgesaActiva:  new Style({
-      image: new Icon({ src: './assets/icons/estaciones/IconoEstacionEmgesaActiva.png', scale: 0.25, anchor: [0.5, 1] })}),
+      image: new Icon({ src: './assets/icons/estaciones/IconoEstacionEmgesaActiva.png', scale: 0.20, anchor: [0.5, 1] })}),
     hidroEmgesaInactiva:  new Style({
-      image: new Icon({ src: './assets/icons/estaciones/IconoEstacionEmgesaInactiva.png',   scale: 0.25, anchor: [0.5, 1] })}),
+      image: new Icon({ src: './assets/icons/estaciones/IconoEstacionEmgesaInactiva.png',   scale: 0.20, anchor: [0.5, 1] })}),
     hidroActiva:  new Style({
-      image: new Icon({ src: './assets/icons/estaciones/IconoEstacionOtrosActiva.png', scale: 0.55, anchor: [0.5, 1] })}),
+      image: new Icon({ src: './assets/icons/estaciones/IconoEstacionOtrosActiva.png', scale: 0.45, anchor: [0.5, 1] })}),
     hidroInactiva:  new Style({
-      image: new Icon({ src: './assets/icons/estaciones/IconoEstacionOtrosInactiva.png',   scale: 0.55, anchor: [0.5, 1] })}),
+      image: new Icon({ src: './assets/icons/estaciones/IconoEstacionOtrosInactiva.png',   scale: 0.45, anchor: [0.5, 1] })}),
     Default : new Style({image: new Circle({
       radius: 30,
       fill: new Fill({color: 'rgba(120, 191, 255, 0.6)'}),
@@ -47,7 +47,7 @@ export class SidebarComponent implements OnInit {
 
   selectedStylesHidro = {
     hidroSelected:  new Style({
-      image: new Icon({ src: './assets/icons/estaciones/IconoSeleccionado.png', scale: 0.75, anchor: [0.5, 1] })}),
+      image: new Icon({ src: './assets/icons/estaciones/IconoSeleccionado.png', scale: 0.45, anchor: [0.5, 1] })}),
     Default : new Style({image: new Circle({
       radius: 20,
       fill: new Fill({color: 'rgba(120, 191, 255, 0.6)'}),
