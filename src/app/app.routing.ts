2a4ff0fff68dd673f5f5c11a8970c92839b20c9e
Diana Calpa <dp.calpa251@gmail.com>
2019-06-24 19:09:16 -0500

    angular json fixed

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,7 @@
-import { MapComponent } from './map/map.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  {path: 'map', component: MapComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
