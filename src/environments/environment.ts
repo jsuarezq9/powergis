7455684bf11e0db216229a407378f19d89c609f2
diego <d_agudelocontreras@hotmail.com>
2019-07-09 10:26:48 -0500

    Auto stash before rebase of "origin/merge/module3"

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   HOST: 'http://10.154.80.177',
-  PRO_TOKEN: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiZ2VzdG9yX2ludGVybm8ifQ.pjwTkG0CrF-vXIKT4hME-lRdFlWv8-wcnr3Pr_QdHo4',
+  PRO_TOKEN: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiZ2VzdG9yX2ludGVybm8iLCJlbWFpbCI6InBvd2VyZ2lzQGVuZWwuY29tIiwiZXhwIjoxNzM1Njg5NjAwfQ.TKH-WNqZabFBx2f38mF_nlZH2IleRGYYSthJghmNLCk',
   APP_JSON: 'application/json'
 };
 
